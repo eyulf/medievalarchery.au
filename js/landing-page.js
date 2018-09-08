576b982855eee3c4a2522ef61d11ce1c2341fb4d
Alex <alex@alexgardner.id.au>
2018-09-08 22:23:21 +1000

    update

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
     var header = $("#header");
-    var height = $(window).height();
+    var height = $("#main").position().top;
+
     header.addClass("no-background");
 
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
+        var height = $("#main").position().top;
 
         if (scroll >= height) {
             header.removeClass("no-background");
